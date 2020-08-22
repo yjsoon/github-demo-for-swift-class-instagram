6e20aea5db5bc386380892f6eaa5b456204ec9ca
YJ Soon <yjsoon@tinkertanker.com>
2020-08-22 10:07:39 +0800

    All functionality added! This is perfect

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        label.text = "GRAM"
+    }
+    
 }
 
